eae72679377931fa469961a7ab71fb361852c7e4
Bhuvanesh66 <bhuvaneshms60@gmail.com>
2026-01-19 16:54:29 +0530

    final commit

--- ecommerce-api/src/main/java/com/example/ecommerce/repository/OrderItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.ecommerce.repository;
-
-import com.example.ecommerce.model.OrderItem;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import java.util.List;
-
-public interface OrderItemRepository extends MongoRepository<OrderItem, String> {
-    List<OrderItem> findByOrderId(String orderId);
-}
